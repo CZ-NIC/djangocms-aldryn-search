f0fbd2aac4c02b9f9932ef44c225c64b2e270383
Zdeněk Böhm <zdenek.bohm@seznam.cz>
2024-11-07 12:25:00 +0100

    Fix publish_cms_page and unpublish_cms_page.

--- aldryn_search/receivers.py
+++ aldryn_search/receivers.py
@@ -4,7 +4,7 @@ Created on Nov 30, 2015
 """
 from django.dispatch.dispatcher import receiver
 
-from cms.models.titlemodels import Title
+from cms.models.pagemodel import Page
 from cms.signals import post_publish, post_unpublish
 
 from .signals import add_to_index, remove_from_index
@@ -12,11 +12,11 @@ from .signals import add_to_index, remove_from_index
 
 @receiver(post_publish, dispatch_uid='publish_cms_page')
 def publish_cms_page(sender, instance, language, **kwargs):
-    title = instance.publisher_public.get_title_obj(language)
-    add_to_index.send(sender=Title, instance=title, object_action='publish')
+    title = instance.get_title(language)
+    add_to_index.send(sender=Page, instance=title, object_action='publish')
 
 
 @receiver(post_unpublish, dispatch_uid='unpublish_cms_page')
 def unpublish_cms_page(sender, instance, language, **kwargs):
-    title = instance.publisher_public.get_title_obj(language)
-    remove_from_index.send(sender=Title, instance=title, object_action='unpublish')
+    title = instance.get_title(language)
+    remove_from_index.send(sender=Page, instance=title, object_action='unpublish')
